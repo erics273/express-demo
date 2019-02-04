28324051b08dd6cd9d1f055b706a3ec1766a8cfe
Eric Schwartz <eric@techtalentsouth.com>
2019-02-04 17:51:38 -0500

    adding xray

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express(),
+
+var AWSXRay = require('aws-xray-sdk');
+app.use(AWSXRay.express.openSegment('MyApp'));
+
 path = require('path'),
 publicDir = path.join(__dirname,'public');
 
 app.use(express.static(publicDir))
+
+app.use(AWSXRay.express.closeSegment());
 
 app.listen(port);
 module.exports = app;
